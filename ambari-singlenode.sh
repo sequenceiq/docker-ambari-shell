dc4d2186bad386c72c391394eedbdbdb8e435dce
lalyos <lalyos@yahoo.com>
2014-06-27 19:36:13 +0200

    specify tag: latest, to minimize intial pull size

--- ambari-singlenode.sh
+++ ambari-singlenode.sh
@@ -6,5 +6,5 @@ curl -LOs j.mp/ambari-singlenode && . ambari-singlenode
 full documentation: https://github.com/sequenceiq/docker-ambari-shell
 USAGE
 
-docker run -d -p 8080 -h amb0.mycorp.kom --name ambari-singlenode sequenceiq/ambari --tag ambari-server=true
-docker run -e BLUEPRINT=single-node-hdfs-yarn --link ambari-singlenode:ambariserver -t --rm --entrypoint /bin/sh sequenceiq/ambari -c /tmp/install-cluster.sh
+docker run -d -p 8080 -h amb0.mycorp.kom --name ambari-singlenode sequenceiq/ambari:latest --tag ambari-server=true
+docker run -e BLUEPRINT=single-node-hdfs-yarn --link ambari-singlenode:ambariserver -t --rm --entrypoint /bin/sh sequenceiq/ambari:latest -c /tmp/install-cluster.sh
