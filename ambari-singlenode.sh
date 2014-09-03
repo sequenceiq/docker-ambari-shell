512e444f487d62a24eda5942b38e081c7adb0219
Janos Matyas <janos.matyas@sequenceiq.com>
2014-09-03 18:37:15 +0200

    Update ambari-singlenode.sh

--- ambari-singlenode.sh
+++ ambari-singlenode.sh
@@ -8,5 +8,5 @@ wget j.mp/ambari-singlenode -O ambari-singlenode && . ambari-singlenode
 full documentation: https://github.com/sequenceiq/docker-ambari-shell
 USAGE
 
-docker run -d -p 8080 -h amb0.mycorp.kom --name ambari-singlenode sequenceiq/ambari:latest --tag ambari-server=true
-docker run -e BLUEPRINT=single-node-hdfs-yarn --link ambari-singlenode:ambariserver -t --rm --entrypoint /bin/sh sequenceiq/ambari:latest -c /tmp/install-cluster.sh
+docker run -d -p 8080 -h amb0.mycorp.kom --name ambari-singlenode sequenceiq/ambari:1.6.0 --tag ambari-server=true
+docker run -e BLUEPRINT=single-node-hdfs-yarn --link ambari-singlenode:ambariserver -t --rm --entrypoint /bin/sh sequenceiq/ambari:1.6.0 -c /tmp/install-cluster.sh
